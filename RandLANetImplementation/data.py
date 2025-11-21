f021a7223efc5fee3bff5685aaf8b9d2fc215657
JamesTheGun <jamedbrunton@gmail.com>
2025-11-21 13:33:26 +1100

    building out data related classes

--- RandLANetImplementation/data.py
+++ RandLANetImplementation/data.py
@@ -1,10 +1,32 @@
 from dataclasses import dataclass
 import pandas as pd
 from constants import HYPER_PARAMETERS, TRAIN_TO_TEST_RATIO
+from typing import List
+import torch
 
 
 def load_df():
     pass
+
+
+def build_lidar_tensor(lidar_data_pd: pd.DataFrame) -> torch.Tensor:
+    pass
+
+
+def build_lidar_window_fold(points: torch.Tensor):
+    N, D = points.shape
+
+
+@dataclass
+class Fold:
+    train_idx: pd.Series
+    val_idx: pd.Series
+
+    def get_train(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data.iloc[self.train_idx]
+
+    def get_val(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data.iloc[self.val_idx]
 
 
 @dataclass
@@ -15,6 +37,7 @@ class modelData:
     train_set: pd.DataFrame = None
     test_set: pd.DataFrame = None
     hyper_params: dict = None
+    fold: List[Fold]
 
     def prepare_hyper_params(self):
         self.hyper_params = HYPER_PARAMETERS
@@ -43,6 +66,9 @@ class modelData:
         pass
 
     def next_fold():
+        pass
+
+    def get_folds():
         pass
 
     def test_test_lebels(test_labels: pd.Series):
