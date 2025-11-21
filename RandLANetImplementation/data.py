6c2c038c8b0ec7bd077e6c9b61afe01dd00b1f7b
JamesTheGun <jamedbrunton@gmail.com>
2025-11-21 12:32:24 +1100

    initial scaffold for RandLANet...

---
+++ RandLANetImplementation/data.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+import pandas as pd
+from constants import HYPER_PARAMETERS, TRAIN_TO_TEST_RATIO
+
+
+def load_df():
+    pass
+
+
+@dataclass
+class modelData:
+    """prepare and store data with methods for retreiving train/test split"""
+
+    data: pd.DataFrame = None
+    train_set: pd.DataFrame = None
+    test_set: pd.DataFrame = None
+    hyper_params: dict = None
+
+    def prepare_hyper_params(self):
+        self.hyper_params = HYPER_PARAMETERS
+
+    def prepare_train_test(self, train_to_test_ratio: float = None):
+        if not train_to_test_ratio:
+            train_to_test_ratio = TRAIN_TO_TEST_RATIO
+        self.train_set, self.test_set = self._get_train_test(
+            self.data, train_to_test_ratio
+        )
+
+    def new_split(self, train_to_test_ratio: float = None):
+        self.prepare_hyper_params()
+        self.prepare_data()
+        self.prepare_train_test(train_to_test_ratio)
+
+    @staticmethod
+    def _get_train_test(data: pd.DataFrame, train_to_test_ratio: float):
+        pass
+
+    @staticmethod
+    def _get_fold(data: pd.DataFrame, ratio: float):
+        pass
+
+    def init_folds():
+        pass
+
+    def next_fold():
+        pass
+
+    def test_test_lebels(test_labels: pd.Series):
+        pass
---
+++ RandLANetImplementation/modules.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+# from torch import
+import pandas as pd
+from constants import TRAIN_TO_TEST_RATIO, HYPER_PARAMETERS
+
+class RandLANet:
+    def __init__(self):
+        self.data = modelData()
+
+    def generate_split():
+        pass
+
+    def train():
+        pass
+
+    def test():
+        pass
